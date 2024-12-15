dd60616b72dea94de6fac2e9592905df8dc0c2da
allesonsales <allesonsales@gmail.com>
2024-12-14 21:32:52 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const toggleTheme = document.getElementById ('toggleTheme')
+const rootHtml = document.documentElement
+
+function changeTheme() {
+    const currentTheme = rootHtml.getAttribute("data-theme")
+    currentTheme == "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark")
+
+        toggleTheme.classList.toggle("bi-brightness-high")
+        toggleTheme.classList.toggle("bi-moon")
+    }
+   
+
+
+toggleTheme.addEventListener('click', changeTheme)
